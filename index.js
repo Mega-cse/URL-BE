90a277ef619c98a136a8648277cff3d022bf4bb9
megavardhini1958@gmail.com <megavardhini1958@gmail.com>
2024-09-08 20:39:17 +0530

    Fix bug in URL shortener and update README

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import cors from 'cors'
 import dotenv from "dotenv";
 import connectDB from './Database/config.js';
-import routes from './Routes/urlRoutes.js';
 import bodyParser from 'body-parser';
-import userRoutes from './Routes/authRouter.js'
+
+import router from './src/Routes/urlRoutes.js';
+import userRoutes from './src/Routes/authRouter.js';
 
 
 dotenv.config()
@@ -13,7 +14,7 @@ connectDB()
 const port=process.env.PORT
 app.use(cors())
 app.use(bodyParser.json());
-app.use('/api',routes)
+app.use('/api',router)
 app.use('/api/user',userRoutes)
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
